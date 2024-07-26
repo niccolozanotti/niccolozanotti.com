30fdfac81985c172c56a1e1c4fba11cfc06e6009
Niccolo' Zanotti <niccolo.zanotti@icloud.com>
2024-07-26 14:25:47 +0200

    Fixed footer links

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/niccolozanotti/niccolozanotti.com",
+      "Other foot-link": "https://youtu.be/dQw4w9WgXcQ?",
     },
   }),
 }
