884ff31c3d805366278bd8915a1abd9853b9d79c
Niccolo' Zanotti <niccolo.zanotti@icloud.com>
2024-08-12 19:08:59 +0200

    Modified header

--- quartz/components/LinksHeader.tsx
+++ quartz/components/LinksHeader.tsx
@@ -13,19 +13,24 @@ export default (() => {
       <div>
         <div id="links-header">
           <span>
-            <img src={"https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Notebook/3D/notebook_3d.png"}></img>
-            <a href="/Uni/Uni">Uni notes</a>
+            <img
+              src={"https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Classical%20building/3D/classical_building_3d.png"}></img>
+            <a href="/Structure">Structure</a>
           </span>
           <span>
-            <img src={"https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Globe%20showing%20europe-africa/3D/globe_showing_europe-africa_3d.png"} alt={"Globe Africa+Europe"}></img>
+            <img
+              src={"https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Notebook/3D/notebook_3d.png"}></img>
+            <a href="/Uni/Uni">Uni</a>
+          </span>
+          <span>
+            <img
+              src={"https://raw.githubusercontent.com/niccolozanotti/niccolozanotti.com/6b995849952c3696472fb37bc0c0c1d98221a167/icons/globe.svg"}
+              alt={"Globe Africa+Europe"}></img>
             <a href="/Research/Research">Research</a>
           </span>
           <span>
-            <img src={"https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Classical%20building/3D/classical_building_3d.png"}></img>
-            <a href="/Structure">Content structure</a>
-          </span>
-          <span>
-            <img src={"https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Face%20with%20monocle/3D/face_with_monocle_3d.png"}></img>
+            <img
+              src={"https://raw.githubusercontent.com/niccolozanotti/niccolozanotti.com/ec15c75e013f7f1f3136688029d59dc222f11f28/icons/person.svg"}></img>
             <a href="/About-me">About me</a>
           </span>
           {/*<span>*/}
@@ -33,7 +38,7 @@ export default (() => {
           {/*  <a href="/relative-path">Maths</a>*/}
           {/*</span>*/}
         </div>
-      <hr style="background-color: var(--gray); border-top: 1px var(--gray) solid; margin-top: 1.3rem"></hr>
+        <hr style="background-color: var(--gray); border-top: 1px var(--gray) solid; margin-top: 1.3rem"></hr>
       </div>
     )
   }
