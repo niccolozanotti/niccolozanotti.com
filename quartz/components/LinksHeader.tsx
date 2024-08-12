8ab076f9000193fa73248f24b36fccfdfd69792e
Niccolo' Zanotti <niccolo.zanotti@icloud.com>
2024-08-12 18:23:29 +0200

    Trying to fix rendering anomaly with internet-sourced emojis in the header.

--- quartz/components/LinksHeader.tsx
+++ quartz/components/LinksHeader.tsx
@@ -13,19 +13,19 @@ export default (() => {
       <div>
         <div id="links-header">
           <span>
-            <img>📚</img>
+            <img src={"https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Notebook/3D/notebook_3d.png"}></img>
             <a href="/Uni/Uni">Uni notes</a>
           </span>
           <span>
-            <img>🌍</img>
+            <img src={"https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Globe%20showing%20europe-africa/3D/globe_showing_europe-africa_3d.png"} alt={"Globe Africa+Europe"}></img>
             <a href="/Research/Research">Research</a>
           </span>
           <span>
-            <img>🏛️</img>
+            <img src={"https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Classical%20building/3D/classical_building_3d.png"}></img>
             <a href="/Structure">Content structure</a>
           </span>
           <span>
-            <img>👤</img>
+            <img src={"https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Face%20with%20monocle/3D/face_with_monocle_3d.png"}></img>
             <a href="/About-me">About me</a>
           </span>
           {/*<span>*/}
