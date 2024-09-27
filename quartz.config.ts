c265c3bf8cb49aa4333f8357204471ca1ce63afe
Niccolo' Zanotti <niccolo.zanotti@icloud.com>
2024-09-27 15:57:21 +0200

    edit(config): change title.

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "📓AI&Climate Notes",
+    pageTitle: "🌱AI4Climate.science",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
